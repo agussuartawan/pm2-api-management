fecb5f5d18c5be068e0e7140fd360774ef98b8e3
om.hairis <hairismahoree@gmail.com>
2024-02-03 04:38:07 +0800

    update bot init

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express"
 import route from "./route/route.js";
-import {botInit} from "./bot/tele-bot.js";
+import bot from "./bot/tele-bot.js";
+
 
 const app = express()
 const port = 8071
 
 app.use(express.json())
 app.use(route)
-app.use(botInit)
+app.use(bot)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
