54f0e33080034a4b155536242cb08ad24417db07
om.hairis <hairismahoree@gmail.com>
2024-02-03 01:48:01 +0800

    add sipwan-dashboard deploy

---
+++ controller/deploy-control.js
@@ -0,0 +1,47 @@
+import { exec } from "child_process"
+
+
+function deploySipwanDashboard(req, res) {
+    let folderName = req.query.folderName
+    let branchName = req.query.branchName
+
+    if (!folderName) {
+       folderName = "sipwan-dashboard"
+    }
+    if (!branchName) {
+        branchName = "main"
+    }
+
+    const dir = `cd /home/admin/ui/${folderName}/staging`
+    const updateRepo = `git pull origin ${branchName}`
+    const pm2run = `mvn install -Pproduction && pm2 delete ${folderName}; pm2 start --name ${folderName} java -- -jar -Dspring.profiles.active=staging "./target/sipwan-payment-dashboard-1.0-SNAPSHOT.jar"`
+    const script = `${dir} && ${updateRepo} && ${pm2run}`
+
+    exec(script, (err, stdout, stderr) => {
+        if (err) {
+            res.status(500).json({
+                status: "error",
+                message: "Internal server error",
+                detail: err.message
+            })
+            return
+        }
+
+        if (stderr) {
+            res.status(500).json({
+                status: "error",
+                message: "Script error",
+                detail: stderr
+            })
+            return
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Start sipwan-dashboard success",
+            detail: stdout
+        })
+    })
+}
+
+export default deploySipwanDashboard
--- route/route.js
+++ route/route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import pm2list from "../pm2-control.js";
+import pm2list from "../controller/pm2-control.js";
+import deploySipwanDashboard from "../controller/deploy-control.js";
 
 const router = express.Router()
 
@@ -8,6 +9,7 @@ router.get("/", (req, res) => {
 })
 
 router.get("/pm2/list", pm2list)
+router.get("/pm2/deploy/sipwan-dashboard", deploySipwanDashboard)
 
 export default router
 
