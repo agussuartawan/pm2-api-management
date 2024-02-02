90c05b5dcb4422f0e7b1000a1337ebe1b0cc417b
om.hairis <hairismahoree@gmail.com>
2024-02-03 04:08:58 +0800

    add tele bot

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import route from "./route/route.js";
+import {botInit} from "./bot/tele-bot.js";
 
 const app = express()
 const port = 8071
 
 app.use(express.json())
 app.use(route)
+app.use(botInit)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
